a473c5d8fbd762ded66c3d5ab154fb7f7534f5e3
mohamedamgad <mishomishmish2@gmail.com>
2025-11-22 23:40:38 +0200

    [feat] implement the authentication service and authentication controller

--- src/main/java/com/example/security/auth/AuthenticationController.java
+++ src/main/java/com/example/security/auth/AuthenticationController.java
@@ -1,4 +1,25 @@
 package com.example.security.auth;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
 public class AuthenticationController {
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody AuthenticationRequest authenticationRequest) {
+        return ResponseEntity.ok(authenticationService.register(authenticationRequest));
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest authenticationRequest) {
+        return ResponseEntity.ok(authenticationService.authenticate(authenticationRequest));
+    }
 }
--- src/main/java/com/example/security/auth/AuthenticationService.java
+++ src/main/java/com/example/security/auth/AuthenticationService.java
@@ -1,4 +1,47 @@
 package com.example.security.auth;
 
+import com.example.security.config.JwtService;
+import com.example.security.user.Role;
+import com.example.security.user.User;
+import com.example.security.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class AuthenticationService {
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(AuthenticationRequest authenticationRequest) {
+        User user = User
+                .builder()
+                .firstName(authenticationRequest.getFirstName())
+                .lastName(authenticationRequest.getLastName())
+                .email(authenticationRequest.getEmail())
+                .password(passwordEncoder.encode(authenticationRequest.getPassword()))
+                .role(Role.USER)
+                .build();
+        userRepository.save(user);
+        String token = jwtService.generateToken(user);
+        return AuthenticationResponse
+                .builder()
+                .token(token)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest authenticationRequest) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authenticationRequest.getEmail(), authenticationRequest.getPassword()));
+        User user = userRepository.findByEmail(authenticationRequest.getEmail()).orElseThrow();
+        String token = jwtService.generateToken(user);
+        return AuthenticationResponse
+                .builder()
+                .token(token)
+                .build();
+    }
 }
